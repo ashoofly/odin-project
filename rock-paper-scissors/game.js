f5c665e7c026f8d8a5ad67f7e6007841b315881f
Angela Hsu <ayh123@gmail.com>
2022-06-03 17:35:35 -0600

    added dom manipulation

--- rock-paper-scissors/game.js
+++ rock-paper-scissors/game.js
@@ -1,8 +1,49 @@
 const options = ["rock", "paper", "scissors"];
+const rockBtn = document.querySelector('#rock');
+const paperBtn = document.querySelector('#paper');
+const scissorsBtn = document.querySelector('#scissors');
+
+const roundNumber = document.querySelector('#round_number');
+const playerScore = document.querySelector('#player_score');
+const computerScore = document.querySelector('#computer_score');
+const roundResult = document.querySelector('#round_result');
+const finalResult = document.querySelector('#final_result');
+
+function incrementRound() {
+  roundNumber.textContent = parseInt(roundNumber.textContent) + 1;
+}
+
+function incrementPlayerScore() {
+  const newScore = parseInt(playerScore.textContent) + 1;
+  playerScore.textContent = newScore;
+  isGameOver('Player', newScore);
+}
+
+function incrementComputerScore() {
+  const newScore = parseInt(computerScore.textContent) + 1;
+  computerScore.textContent = newScore;
+  isGameOver('Computer', newScore);
+}
 
 function computerPlay() {
   return options[Math.floor(Math.random()*options.length)];
 }
+
+rockBtn.addEventListener('click', function(e) {
+  playRound('rock', computerPlay());
+  incrementRound();
+});
+
+paperBtn.addEventListener('click', function(e) {
+  playRound('paper', computerPlay());
+  incrementRound();
+});
+
+scissorsBtn.addEventListener('click', function(e) {
+  playRound('scissors', computerPlay());
+  incrementRound();
+});
+
 
 function playRound(playerChoice, computerSelection) {
   const playerSelection = playerChoice.toLowerCase();
@@ -10,58 +51,40 @@ function playRound(playerChoice, computerSelection) {
   let result = `${playerSelection} VS ${computerSelection}`
 
   if (playerSelection == computerSelection) {
-    console.log(`${result}: tie`);
-    return [0, 0];
+    roundResult.textContent = `${result}: tie`;
 
   } else if (playerSelection == "rock") {
     if (computerSelection == "scissors") {
-      console.log(`${result}: win`);
-      return [1, 0]
+      roundResult.textContent = `${result}: win`;
+      incrementPlayerScore();
 
     } else if (computerSelection == "paper") {
-      console.log(`${result}: loss`);
-      return [0, 1];
+      roundResult.textContent = `${result}: loss`;
+      incrementComputerScore();
     }  
   } else if (playerSelection == "scissors") {
     if (computerSelection == "rock") {
-      console.log(`${result}: loss`);
-      return [0, 1];
+      roundResult.textContent = `${result}: loss`;
+      incrementComputerScore();
 
     } else if (computerSelection == "paper") {
-      console.log(`${result}: win`);
-      return [1, 0];
+      roundResult.textContent = `${result}: win`;
+      incrementPlayerScore();
     }
   } else if (playerSelection == "paper") {
     if (computerSelection == "rock") {
-      console.log(`${result}: win`);
-      return [1, 0];
+      roundResult.textContent = `${result}: win`;
+      incrementPlayerScore();
 
     } else if (computerSelection == "scissors") {
-      console.log(`${result}: loss`);
-      return [0, 1];
+      roundResult.textContent = `${result}: loss`;
+      incrementComputerScore();
     }
   }
 }
 
-function isGameOver(scores) {
-  return scores[0] >=3 || scores[1] >= 3;
-}
-
-function game() {
-  let scores = [0, 0];
-  let i = 0;
-  while (!isGameOver(scores)) {
-    i++;
-    let playerChoice = prompt("Rock, paper, or scissors?");
-    const computerSelection = computerPlay();
-    let roundScore = playRound(playerChoice, computerSelection);
-    scores[0] += roundScore[0];
-    scores[1] += roundScore[1];
-    console.log(`\nRound ${i}: You: ${scores[0]}. Computer: ${scores[1]}`);
+function isGameOver(player, score) {
+  if (score >=5) {
+    finalResult.textContent = `${player} WINS!!!`;
   }
-  console.log(`Final number of rounds: ${i}`);
-  console.log(`Final score:`);
-  console.log(`\nYou: ${scores[0]}. Computer: ${scores[1]}`);
 }
-
-game();
